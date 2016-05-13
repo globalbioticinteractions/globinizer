532cf6ef28f40dfdc55554f2e9be1340c8d99ef6
Jorrit Poelen <jhpoelen@gmail.com>
2016-05-12 18:09:57 -0700

    added logging

--- check-dataset.sh
+++ check-dataset.sh
@@ -12,6 +12,8 @@ export REPO_NAME=$1
 export GLOBI_VERSION=`curl -s https://api.github.com/repos/jhpoelen/eol-globi-data/releases/latest | grep -o '[0-9]\.[0-9]\.[0-9]' | head -n 1`
 export GLOBI_DATA_REPO_MASTER="https://raw.githubusercontent.com/${REPO_NAME}/master"
 
+echo Checking [${GLOBI_DATA_REPO_MASTER}] using GloBI tools version [$sGLOBI_VERSION}]. 
+
 wget http://globi.s3.amazonaws.com/release/org/eol/eol-globi-data-tool/${GLOBI_VERSION}/eol-globi-data-tool-${GLOBI_VERSION}-jar-with-dependencies.jar -O globi-tool.jar
 
 java -cp globi-tool.jar org.eol.globi.tool.GitHubRepoCheck ${REPO_NAME} ${GLOBI_DATA_REPO_MASTER}
