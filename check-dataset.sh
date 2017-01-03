c965acc7abeb2aa63bdb07e1cd90249d1d06e0a4
Jorrit Poelen <jhpoelen@gmail.com>
2017-01-03 11:47:51 -0800

    another attempt to force usage of jdk8

--- check-dataset.sh
+++ check-dataset.sh
@@ -12,11 +12,10 @@ export REPO_NAME=$1
 #export GLOBI_VERSION=`curl -s https://api.github.com/repos/jhpoelen/eol-globi-data/releases/latest | grep -o '[0-9]\.[0-9]\.[0-9]' | head -n 1`
 export GLOBI_VERSION=0.8.0
 export GLOBI_DATA_REPO_MASTER="https://raw.githubusercontent.com/${REPO_NAME}/master"
-
+export JAVA_HOME=
 echo Checking [${GLOBI_DATA_REPO_MASTER}] using GloBI tools version [${GLOBI_VERSION}]. 
 
 wget http://globi.s3.amazonaws.com/release/org/eol/eol-globi-data-tool/${GLOBI_VERSION}/eol-globi-data-tool-${GLOBI_VERSION}-jar-with-dependencies.jar -O globi-tool.jar
 
-jdk_switcher use oraclejdk8
-java -cp globi-tool.jar org.eol.globi.tool.GitHubRepoCheck ${REPO_NAME} ${GLOBI_DATA_REPO_MASTER}
-
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+${JAVA_HOME}/jre/bin/java -cp globi-tool.jar org.eol.globi.tool.GitHubRepoCheck ${REPO_NAME} ${GLOBI_DATA_REPO_MASTER}
