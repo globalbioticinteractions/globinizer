993f450bfd8be489a693fe3bdea02ba7501b3d9f
jhpoelen <jhpoelen@xs4all.nl>
2017-09-13 18:59:39 -0700

    switch to jitpack hosted globinizer

--- check-dataset.sh
+++ check-dataset.sh
@@ -10,12 +10,12 @@
 
 export REPO_NAME=$1
 #export GLOBI_VERSION=`curl -s https://api.github.com/repos/jhpoelen/eol-globi-data/releases/latest | grep -o '[0-9]\.[0-9]\.[0-9]' | head -n 1`
-export GLOBI_VERSION=0.8.15
+export GLOBI_VERSION=master-SNAPSHOT
 export GLOBI_DATA_REPO_MASTER="https://raw.githubusercontent.com/${REPO_NAME}/master"
 
 echo Checking [${GLOBI_DATA_REPO_MASTER}] using GloBI tools version [${GLOBI_VERSION}]. 
 
-wget http://globi.s3.amazonaws.com/release/org/eol/eol-globi-data-tool/${GLOBI_VERSION}/eol-globi-data-tool-${GLOBI_VERSION}-jar-with-dependencies.jar -O globi-tool.jar
+wget https://jitpack.io/com/github/globalbioticinteractions/globinizer/${GLOBI_VERSION}/globinizer-${GLOBI_VERSION}-jar-with-dependencies.jar -O globi-tool.jar
 
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 ${JAVA_HOME}/jre/bin/java -cp globi-tool.jar org.eol.globi.tool.GitHubRepoCheck ${REPO_NAME} ${GLOBI_DATA_REPO_MASTER}
