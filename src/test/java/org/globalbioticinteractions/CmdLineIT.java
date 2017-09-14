bdf88ba19107bb5c5ab30796fcce26fb10b3c163
jhpoelen <jhpoelen@xs4all.nl>
2017-09-14 09:00:51 -0700

    separate unit and integration tests

---
+++ src/test/java/org/globalbioticinteractions/CmdLineIT.java
@@ -0,0 +1,85 @@
+package org.globalbioticinteractions;
+
+import com.beust.jcommander.JCommander;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CmdLineIT {
+
+    @Test
+    public void list() {
+        JCommander jc = new CmdLine().buildCommander();
+
+        jc.parse("list");
+
+        Assert.assertEquals(jc.getParsedCommand(), "list");
+
+        JCommander actual = jc.getCommands().get(jc.getParsedCommand());
+        Assert.assertEquals(actual.getObjects().size(), 1);
+        Assert.assertEquals(actual.getObjects().get(0).getClass(), CmdLine.CommandList.class);
+
+        if (actual.getObjects().get(0) instanceof Runnable) {
+            ((Runnable) actual.getObjects().get(0)).run();
+        }
+    }
+
+    @Test
+    public void update() {
+        JCommander jc = new CmdLine().buildCommander();
+        jc.parse("update", "--cache-dir=./bla");
+        assertUpdateCmd(jc);
+
+        jc = new CmdLine().buildCommander();
+        jc.parse("update", "-c", "./bla");
+        assertUpdateCmd(jc);
+
+        jc = new CmdLine().buildCommander();
+        jc.parse("-c", "./bla", "update");
+        assertUpdateCmd(jc);
+    }
+
+    @Test
+    public void updateRun() {
+        JCommander jc = new CmdLine().buildCommander();
+        jc.parse("update", "--cache-dir=./target/tmp-dataset");
+
+        JCommander actual = jc.getCommands().get(jc.getParsedCommand());
+        Assert.assertEquals(actual.getObjects().size(), 1);
+        Assert.assertEquals(actual.getObjects().get(0).getClass(), CmdLine.CommandUpdate.class);
+
+        if (actual.getObjects().get(0) instanceof Runnable) {
+            ((Runnable) actual.getObjects().get(0)).run();
+        }
+
+    }
+
+    @Test
+    public void testRun() {
+        JCommander jc = new CmdLine().buildCommander();
+        jc.parse("test", "--cache-dir=./target/tmp-dataset", "globalbioticinteractions/template-dataset");
+
+        JCommander actual = jc.getCommands().get(jc.getParsedCommand());
+        Assert.assertEquals(actual.getObjects().size(), 1);
+        Assert.assertEquals(actual.getObjects().get(0).getClass(), CmdLine.CommandTest.class);
+
+        if (actual.getObjects().get(0) instanceof Runnable) {
+            ((Runnable) actual.getObjects().get(0)).run();
+        }
+
+    }
+
+    private void assertUpdateCmd(JCommander jc) {
+        Assert.assertEquals(jc.getParsedCommand(), "update");
+
+        JCommander actual = jc.getCommands().get(jc.getParsedCommand());
+        Assert.assertEquals(actual.getObjects().size(), 1);
+        Object cmd = actual.getObjects().get(0);
+        Assert.assertEquals(cmd.getClass(), CmdLine.CommandUpdate.class);
+        assertThat(((CmdLine.CommandUpdate) cmd).getCacheDir(), is("./bla"));
+    }
+
+
+}
