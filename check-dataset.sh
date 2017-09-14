36d868e753aa9936fe2bde5aa995ac83a1c1184d
jhpoelen <jhpoelen@xs4all.nl>
2017-09-13 19:28:30 -0700

    attempt to get check-dataset.sh to use most recent release

--- check-dataset.sh
+++ check-dataset.sh
@@ -9,8 +9,7 @@
 #      ./check-dataset.sh globalbioticinteractions/template-dataset
 
 export REPO_NAME=$1
-#export GLOBI_VERSION=`curl -s https://api.github.com/repos/jhpoelen/eol-globi-data/releases/latest | grep -o '[0-9]\.[0-9]\.[0-9]' | head -n 1`
-export GLOBI_VERSION=master-SNAPSHOT
+export GLOBI_VERSION=`curl -s https://api.github.com/repos/globalbioticinteractions/globinizer/releases/latest | grep -o '[0-9]\.[0-9]' | head -n 1`
 export GLOBI_DATA_REPO_MASTER="https://raw.githubusercontent.com/${REPO_NAME}/master"
 
 echo Checking [${GLOBI_DATA_REPO_MASTER}] using GloBI tools version [${GLOBI_VERSION}]. 
