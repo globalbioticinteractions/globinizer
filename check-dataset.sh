07a736673cfc24b9fc402f522d79a976da92656b
jhpoelen <jhpoelen@xs4all.nl>
2017-09-16 02:25:43 -0700

    bump version

--- check-dataset.sh
+++ check-dataset.sh
@@ -9,13 +9,13 @@
 #      ./check-dataset.sh globalbioticinteractions/template-dataset
 
 export REPO_NAME=$1
-#export GLOBI_VERSION=`curl -s https://api.github.com/repos/globalbioticinteractions/globinizer/releases/latest | grep -o '[0-9]\.[0-9]' | head -n 1`
-export GLOBI_VERSION=0.3
-export GLOBI_DATA_REPO_MASTER="https://raw.githubusercontent.com/${REPO_NAME}/master"
+#export ELTON_VERSION=`curl -s https://api.github.com/repos/globalbioticinteractions/globinizer/releases/latest | grep -o '[0-9]\.[0-9]' | head -n 1`
+export ELTON_VERSION=0.3.4
+export ELTON_DATA_REPO_MASTER="https://raw.githubusercontent.com/${REPO_NAME}/master"
 
-echo Checking [${GLOBI_DATA_REPO_MASTER}] using GloBI tools version [${GLOBI_VERSION}]. 
+echo Checking [${ELTON_DATA_REPO_MASTER}] using Elton version [${ELTON_VERSION}]. 
 
-export URL_PREFIX="https://jitpack.io/com/github/globalbioticinteractions/globinizer/${GLOBI_VERSION}/globinizer-${GLOBI_VERSION}"
+export URL_PREFIX="https://jitpack.io/com/github/globalbioticinteractions/elton/${ELTON_VERSION}/elton-${ELTON_VERSION}"
 
 # poke jitpack to build if one is not available yet
 curl -I "${URL_PREFIX}.pom"
