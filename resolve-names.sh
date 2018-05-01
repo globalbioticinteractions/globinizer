5ef54d1b8f57abc0bd156d47f4f5f59ef1d551f3
Jorrit Poelen <jhpoelen@gmail.com>
2018-05-01 13:10:19 -0700

    attempt to update apt before installing kafkacat

--- resolve-names.sh
+++ resolve-names.sh
@@ -68,6 +68,7 @@ cat names_orig.tsv | sort | uniq | gzip > names_orig_uniq.tsv.gz
 
 # notify GloBI
 echo notifying GloBI of names
+sudo apt-get -qq update
 sudo apt-get install kafkacat
 zcat names_orig_uniq.tsv.gz | kafkacat -b 178.63.23.174 -t names
 
