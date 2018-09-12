3f53ce7610f887c33d9dc5f25d7731a132bc91b8
jhpoelen <jhpoelen@xs4all.nl>
2018-09-12 14:48:32 -0700

    check local only

--- check-dataset.sh
+++ check-dataset.sh
@@ -10,7 +10,7 @@
 set -e
 
 export REPO_NAME=$1
-export ELTON_VERSION=0.5.1
+export ELTON_VERSION=0.5.2
 export ELTON_DATA_REPO_MASTER="https://raw.githubusercontent.com/${REPO_NAME}/master"
 
 echo Checking [${ELTON_DATA_REPO_MASTER}] using Elton version [${ELTON_VERSION}]. 
@@ -21,4 +21,6 @@ export URL_PREFIX="http://depot.globalbioticinteractions.org/release/org/globalb
 wget ${URL_PREFIX}-jar-with-dependencies.jar -O elton.jar
 
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-${JAVA_HOME}/jre/bin/java -jar elton.jar check ${REPO_NAME}
+#${JAVA_HOME}/jre/bin/java -jar elton.jar check ${REPO_NAME}
+# check local only for now
+${JAVA_HOME}/jre/bin/java -jar elton.jar check
