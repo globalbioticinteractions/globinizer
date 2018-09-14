26cd0c257730b0e880628efb0d0c9df5db90ee9c
jhpoelen <jhpoelen@xs4all.nl>
2018-09-13 17:30:29 -0700

    remove reference to jitpack

--- check-dataset.sh
+++ check-dataset.sh
@@ -18,7 +18,6 @@ echo Checking [${ELTON_DATA_REPO_MASTER}] using Elton version [${ELTON_VERSION}]
 
 export URL_PREFIX="http://depot.globalbioticinteractions.org/release/org/globalbioticinteractions/elton/${ELTON_VERSION}/elton-${ELTON_VERSION}"
 
-# poke jitpack to build if one is not available yet
 wget ${URL_PREFIX}-jar-with-dependencies.jar -O elton.jar
 
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
