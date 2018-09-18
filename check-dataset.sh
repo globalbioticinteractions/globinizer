436930bf8b0377fc8fd31f94d1316efa03228a92
jhpoelen <jhpoelen@xs4all.nl>
2018-09-17 18:40:44 -0700

    revert to java -jar

--- check-dataset.sh
+++ check-dataset.sh
@@ -10,6 +10,12 @@
 set -e
 set -x
 
+if [ -z $TRAVIS ]; then 
+  export JAVA_HOME=/usr/lib/jvm/java-8-oracle;
+fi
+
+export JAVA=${JAVA_HOME}/jre/bin/java
+
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export REPO_NAME=$1
 export ELTON_VERSION=0.5.4
@@ -19,7 +25,6 @@ echo Checking [${ELTON_DATA_REPO_MASTER}] using Elton version [${ELTON_VERSION}]
 
 export URL_PREFIX="http://depot.globalbioticinteractions.org/release/org/globalbioticinteractions/elton/${ELTON_VERSION}/elton-${ELTON_VERSION}"
 
-wget ${URL_PREFIX}-jar-with-dependencies.jar -O elton
+wget ${URL_PREFIX}-jar-with-dependencies.jar -O elton.jar
 
-sudo chmod +x elton
-./elton check
+$JAVA -Xmx1G -jar elton.jar check
