adaada2f0d496bd9af453c01357419d157adceb2
jhpoelen <jhpoelen@xs4all.nl>
2019-05-20 22:45:15 -0700

    bump elton version

--- check-dataset.sh
+++ check-dataset.sh
@@ -13,7 +13,7 @@ set -x
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export JAVA=${JAVA_HOME}/jre/bin/java
 export REPO_NAME=$1
-export ELTON_VERSION=0.5.6
+export ELTON_VERSION=0.5.9
 export ELTON_DATA_REPO_MASTER="https://raw.githubusercontent.com/${REPO_NAME}/master"
 
 echo Checking [${ELTON_DATA_REPO_MASTER}] using Elton version [${ELTON_VERSION}]. 
