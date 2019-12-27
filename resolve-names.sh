ad9fd5ee413efbd9cb4ca4b14ac0c31b69892412
jhpoelen <jhpoelen@xs4all.nl>
2019-12-27 14:06:20 -0800

    make review output a little friendlier

--- resolve-names.sh
+++ resolve-names.sh
@@ -7,7 +7,7 @@
 # 
 #   example:
 #      ./check-dataset.sh globalbioticinteractions/template-dataset
-set -e
+#set -e
 
 export REPO_NAME=$1
 export ELTON_VERSION=0.6.1
@@ -17,23 +17,32 @@ echo Reviewing [${ELTON_DATA_REPO_MASTER}] using Elton version [${ELTON_VERSION}
 
 export URL_PREFIX="https://github.com/globalbioticinteractions/elton/releases/download/${ELTON_VERSION}"
 
-wget --quiet ${URL_PREFIX}/elton.jar -O elton.jar
+#wget --quiet ${URL_PREFIX}/elton.jar -O elton.jar
 
-java -Xmx1G -jar elton.jar check > review.tsv
+java -Xmx4G -jar elton.jar check > review.tsv
 REVIEW_RESULT=$?
 
 cat review.tsv | gzip > review.tsv.gz
-zcat review.tsv.gz | tail
 
-curl -F "file=@review.tsv.gz" https://file.io
+echo -e "\nReview included:"
+zcat review.tsv.gz | tail -n3 | cut -f5 | sed s/^/\ \ -\ /g
+echo -e "\n"
+
+curl -F "file=@review.tsv.gz" https://file.io 
+
+echo -e "\n\n"
 
 if [ $REVIEW_RESULT -gt 0 ]
 then
-  echo "[$REPO_NAME] has reviewer comments, including:"
-  zcat review.tsv.gz | tail -n+2 | cut -f5 | sort | uniq -c | sort -nr
-  echo "For full review, please install GloBI's Elton via https://github.com/globalbioticinteractions/elton and run \"elton update $REPO_NAME && elton check $REPO_NAME > review.tsv\""
+  echo -e "[$REPO_NAME] has the following reviewer comments:"
+  zcat review.tsv.gz | tail -n+2 | cut -f5 | tac | tail -n+5 | sort | uniq -c | sort -nr
+  echo -e "\nIf https://file.io link above no longer works, access a full review by:"
+  echo "  - installing GloBI's Elton via https://github.com/globalbioticinteractions/elton"
+  echo "  - running \"elton update $REPO_NAME && elton check $REPO_NAME > review.tsv\""
+  echo "  - inspecting review.tsv"
+  echo -e "\nPlease email info@globalbioticinteractions.org for questions/ comments."
 else
-  echo "Hurray! [$REPO_NAME] passed the GloBI review."
+  echo -e "Hurray! [$REPO_NAME] passed the GloBI review."
 fi
 
 exit $REVIEW_RESULT
