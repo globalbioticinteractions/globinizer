2c6022f79fffaf95954ec04a19216db65d5b934c
jhpoelen <jhpoelen@xs4all.nl>
2020-02-22 13:20:19 -0800

    more artifacts tweaks

--- resolve-names.sh
+++ resolve-names.sh
@@ -55,7 +55,9 @@ fi
 if [[ -n ${ARTIFACTS_KEY} ]] && [[ -n ${ARTIFACTS_SECRET} ]] && [[ -n ${ARTIFACTS_BUCKET} ]]
 then
   echo "got artifacts config: ${ARTIFACTS_BUCKET}"
-  artifacts upload --target-paths "reviews/$TRAVIS_REPO_SLUG" review.tsv.gz 
+  ARTIFACTS_DEBUG=1
+  ARTIFACTS_PATHS=reviews/$TRAVIS_REPO_SLUG
+  artifacts upload review.tsv.gz 
   echo "see also https://depot.globalbioticinteractions.org/reviews/$TRAVIS_REPO_SLUG/reviews.tsv.gz"
 else
   echo "no artifacts found, use file.io instead"
