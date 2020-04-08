a2dfcfcf5d64f9581c9477210563b13f4ac46fd8
jhpoelen <jhpoelen@xs4all.nl>
2020-04-08 13:23:54 -0700

    reduce artifacts logging; update download instructions

--- resolve-names.sh
+++ resolve-names.sh
@@ -56,12 +56,12 @@ function upload_file_io {
 if [[ -n $(which artifacts) ]] && [[ -n ${ARTIFACTS_KEY} ]] && [[ -n ${ARTIFACTS_SECRET} ]] && [[ -n ${ARTIFACTS_BUCKET} ]]
 then
   echo "got artifacts config"
-  artifacts upload --target-paths "reviews/$TRAVIS_REPO_SLUG" review.tsv.gz 
-  echo "see also https://depot.globalbioticinteractions.org/reviews/$TRAVIS_REPO_SLUG/review.tsv.gz"
+  artifacts upload --quiet --target-paths "reviews/$TRAVIS_REPO_SLUG" review.tsv.gz
+  echo -e "\nFor a detailed review, please download:\nhttps://depot.globalbioticinteractions.org/reviews/$TRAVIS_REPO_SLUG/review.tsv.gz"
   
   java -Xmx4G -jar elton.jar interactions | gzip > indexed-interactions.tsv.gz
-  artifacts upload --target-paths "reviews/$TRAVIS_REPO_SLUG" indexed-interactions.tsv.gz
-  echo "and https://depot.globalbioticinteractions.org/reviews/$TRAVIS_REPO_SLUG/indexed-interactions.tsv.gz"
+  artifacts upload --quiet --target-paths "reviews/$TRAVIS_REPO_SLUG" indexed-interactions.tsv.gz
+  echo -e "\nFor a list of indexed interactions, please download:\nhttps://depot.globalbioticinteractions.org/reviews/$TRAVIS_REPO_SLUG/indexed-interactions.tsv.gz\n"
 else
   upload_file_io
 fi
