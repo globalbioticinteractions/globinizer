67abcbf1452daef6b309a972636f75f48a0463a0
jhpoelen <jhpoelen@xs4all.nl>
2020-04-13 18:16:23 -0700

    use /dev/null to silence apt-get

--- check-dataset.sh
+++ check-dataset.sh
@@ -73,7 +73,7 @@ function upload_file_io {
 }
 
 
-sudo apt-get -q update && apt-get -q install awscli -y 
+sudo apt-get -q update && sudo apt-get -q install awscli -y > /dev/null
 
 # atttempt to use travis artifacts tool if available
 if [[ -n $(which aws) ]] && [[ -n ${ARTIFACTS_KEY} ]] && [[ -n ${ARTIFACTS_SECRET} ]] && [[ -n ${ARTIFACTS_BUCKET} ]]
