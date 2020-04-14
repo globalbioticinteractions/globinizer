809f6ab7699e9b56724e879c9656339b6b83263b
jhpoelen <jhpoelen@xs4all.nl>
2020-04-13 17:26:27 -0700

    experiment: use aws cli

--- check-dataset.sh
+++ check-dataset.sh
@@ -73,7 +73,8 @@ function upload_file_io {
 }
 
 
-sudo apt install awscli
+sudo apt-get update
+sudo apt-get install awscli -y
 
 # atttempt to use travis artifacts tool if available
 if [[ -n $(which aws) ]] && [[ -n ${ARTIFACTS_KEY} ]] && [[ -n ${ARTIFACTS_SECRET} ]] && [[ -n ${ARTIFACTS_BUCKET} ]]
