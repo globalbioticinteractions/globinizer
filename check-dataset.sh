24dd9bbc8b388e6847538e3c75d6875a9f7640bb
jhpoelen <jhpoelen@xs4all.nl>
2022-04-22 17:54:18 -0500

    parse names first using gnparser to make [Acer sp.] -> [Acer]

--- check-dataset.sh
+++ check-dataset.sh
@@ -185,6 +185,7 @@ function resolve_names {
   local RESOLVED=indexed-names-resolved-$2.tsv.gz
   echo -e "\n--- [$2] start ---\n"
   time cat $1 | gunzip | tail -n+2 | sort | uniq\
+    | ${NOMER_CMD} replace gn-parse\
     | ${NOMER_CMD} append $2 --include-header\
     | gzip > $RESOLVED
   echo [$2] resolved $(cat $RESOLVED | gunzip | tail -n+2 | grep -v NONE | wc -l) out of $(cat $RESOLVED | gunzip | tail -n+2 | wc -l) names.
