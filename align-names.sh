995b627c9883901d3e2c5d7740549ef2a00b904c
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-01-27 09:16:28 -0600

    use flag instead of comments to enable/disable taxonomy used for alignment

--- align-names.sh
+++ align-names.sh
@@ -157,7 +157,7 @@ function configure_preston {
 }
 
 function configure_nomer {
-  local TAXONOMY_IDS=$(cat README.md | yq --front-matter=extract --header-preprocess '.taxonomies[].id' | sort | uniq)
+  local TAXONOMY_IDS=$(cat README.md | yq --front-matter=extract --header-preprocess '.taxonomies[] | select(.["enabled"] != false) | .id' | sort | uniq)
   if [ $(echo ${TAXONOMY_IDS} | grep -v null | tr ' ' '\n' | wc -l) -gt 0 ]
   then
     NOMER_MATCHERS=${TAXONOMY_IDS}
