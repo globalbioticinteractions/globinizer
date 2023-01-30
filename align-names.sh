5ce512b4b1d85022d3290c6e58497a3007533c68
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-01-30 15:20:19 -0600

    introduce enabling/disabling for datasets; related to https://github.com/globalbioticinteractions/name-alignment-template/issues/10

--- align-names.sh
+++ align-names.sh
@@ -227,10 +227,10 @@ echo -e "\nReview of [${REPO_NAME}] started at [$(date -Iseconds)]." | tee_readm
 
 if [ $(cat README.md | yq --front-matter=extract --header-preprocess '.datasets[].url' | wc -l) -gt 0 ]
 then
-  export TSV_LOCAL=$(cat README.md | yq --front-matter=extract --header-preprocess '.datasets[] | select(.type == "text/tab-separated-values") | .url' | grep -v -P "^http[s]{0,1}://") 
-  export CSV_LOCAL=$(cat README.md | yq --front-matter=extract --header-preprocess '.datasets[] | select(.type == "text/csv") | .url' | grep -v -P "^http[s]{0,1}://") 
-  export DWCA_REMOTE=$(cat README.md | yq --front-matter=extract --header-preprocess '.datasets[] | select(.type == "application/dwca" or .type == "application/rss+xml") | .url' | grep -P "^http[s]{0,1}://") 
-  export NOMER_CATALOGS=$(cat README.md | yq --front-matter=extract --header-preprocess '.datasets[] | select(.type == "application/nomer") | .id' | grep -Po "[a-z]+$") 
+  export TSV_LOCAL=$(cat README.md | yq --front-matter=extract --header-preprocess '.datasets[] | select(.["enabled"] != false) | select(.type == "text/tab-separated-values") | .url' | grep -v -P "^http[s]{0,1}://") 
+  export CSV_LOCAL=$(cat README.md | yq --front-matter=extract --header-preprocess '.datasets[] | select(.["enabled"] != false) | select(.type == "text/csv") | .url' | grep -v -P "^http[s]{0,1}://") 
+  export DWCA_REMOTE=$(cat README.md | yq --front-matter=extract --header-preprocess '.datasets[] | select(.["enabled"] != false) | select(.type == "application/dwca" or .type == "application/rss+xml") | .url' | grep -P "^http[s]{0,1}://") 
+  export NOMER_CATALOGS=$(cat README.md | yq --front-matter=extract --header-preprocess '.datasets[] | select(.["enabled"] != false) | select(.type == "application/nomer") | .id' | grep -Po "[a-z]+$") 
 else 
   export TSV_LOCAL=$(ls -1 *.txt *.tsv)
   export CSV_LOCAL=$(ls -1 *.csv)
