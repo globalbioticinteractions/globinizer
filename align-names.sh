b8ca8cec1f0e8cbd2e48089ce894ceaa46c81ae6
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-02-21 13:53:20 -0600

    add blank extra columns for parse results

--- align-names.sh
+++ align-names.sh
@@ -66,7 +66,7 @@ _EOF_
 
 function names_aligned_header {
   echo "$(cat <<_EOF_
-providedExternalId	providedName	parseRelation	parsedExternalId	parsedName	parsedAuthority	7	8	9	10	11	12	13	alignRelation	alignedExternalId	alignedName	alignedAuthority	alignedRank	alignedCommonNames	alignedPath	alignedPathIds	alignedPathNames	23	alignedUrl
+providedExternalId	providedName	parseRelation	parsedExternalId	parsedName	parsedAuthority	7	8	9	10	11	12	13	14	15	alignRelation	alignedExternalId	alignedName	alignedAuthority	alignedRank	alignedCommonNames	alignedPath	alignedPathIds	alignedPathNames	23	alignedUrl
 _EOF_
 )"
 }
