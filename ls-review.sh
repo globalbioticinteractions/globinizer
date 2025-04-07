20f8de6730c6ea0cfc6afdce93bec8f6bf3fc289
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-04-07 17:41:29 -0500

    use gzipped resources in Zenodo publication

--- ls-review.sh
+++ ls-review.sh
@@ -12,7 +12,6 @@ namespace=${1:-aeiche01/MojaveFoodWeb}
 
 curl "https://depot.globalbioticinteractions.org/reviews/${namespace}/README.txt"\
  | grep "https://depot.globalbioticinteractions.org/reviews/"\
- | sed 's+[.]gz$++'\
  | sort\
  | uniq
 
