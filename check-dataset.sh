3bea175520addfe0acbd4e1cf442280e6646b462
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-05-05 18:24:18 -0500

    drop the quotes

--- check-dataset.sh
+++ check-dataset.sh
@@ -969,7 +969,7 @@ else
   ${ELTON_CMD} prov ${ELTON_OPTS} ${REPO_NAME} | ${ELTON_CMD} tee ${ELTON_OPTS} | ${PRESTON_CMD} append ${PRESTON_OPTS}
 fi
 
-if [[ "${REVIEW_SCRIPT}" != "$(readlink -f check-dataset.sh)" ]]; then
+if [[ ${REVIEW_SCRIPT} != $(readlink -f check-dataset.sh) ]]; then
   echo "include the review script [${REVIEW_SCRIPT}]"
   cat "${REVIEW_SCRIPT}" > check-dataset.sh
 else 
